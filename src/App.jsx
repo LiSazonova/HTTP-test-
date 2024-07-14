d283eaf23347e98dad2c9ab97a88774cc6e5693a
Lili Sazonova <lilissazonova@gmail.com>
2024-07-14 18:18:43 +0300

    add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import ArticleList from './components/ArticleList/ArticleList';
 import s from './App.module.css';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
-      const response = await axios.get(
-        'https://hn.algolia.com/api/v1/search?query=react',
-      );
-      setArticles(response.data.hits);
+      try {
+        // 1. Встановлюємо індикатор в true перед запитом
+        setLoading(true);
+        const response = await axios.get(
+          'https://hn.algolia.com/api/v1/search?query=react',
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        // Тут будемо обробляти помилку
+      } finally {
+        // 2. Встановлюємо індикатор в false після запиту
+        setLoading(false);
+      }
     }
 
     fetchArticles();
@@ -20,7 +31,17 @@ const App = () => {
   return (
     <div>
       <h1 className={s.title}>Latest articles</h1>
-      {articles.length > 0 && <ArticleList items={articles} />}
+      {loading ? (
+        <ClipLoader
+          color={'#747474'}
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        articles.length > 0 && <ArticleList items={articles} />
+      )}
     </div>
   );
 };
